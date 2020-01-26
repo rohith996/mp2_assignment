302b1bb7c3204e03f386aeb09a6fbb7176ed6564
Prasad2083 <alkeshalli.b3@gmail.com>
2020-01-25 21:19:28 -0500

    navigation implemented

---
+++ Weather_app/app/src/main/java/com/example/weather_data/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.weather_data;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    public NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runNavigation();
+    }
+
+    private void runNavigation() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        navController = Navigation.findNavController(this, R.id.nav_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationUI.setupWithNavController(navigationView,navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(drawerLayout,Navigation.findNavController(this,R.id.nav_fragment));
+    }
+
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawers();
+        int id = menuItem.getItemId();
+        switch (id)
+        {
+            case R.id.montreal_menu:
+                navController.navigate(R.id.montreal_frag);
+                break;
+            case R.id.delhi_menu:
+                navController.navigate(R.id.delhi_frag);
+                break;
+        }
+        return true;
+    }
+}
---
+++ Weather_app/app/src/main/java/com/example/weather_data/Montreal_Fragment.java
@@ -0,0 +1,31 @@
+package com.example.weather_data;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Montreal_Fragment extends Fragment {
+
+
+    public Montreal_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_montreal, container, false);
+    }
+
+}
